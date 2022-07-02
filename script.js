75f0af387335289d26dffd07ce491d795f330912
Nenomancer <nebojsa.kovacevik@gmail.com>
2022-07-02 23:45:53 +0200

    Add set resolution functionality

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 const container = document.querySelector(".container");
-
+const gridSizeInput = document.querySelector("#grid-size");
+const gridSizeButton = document.querySelector("#grid-size-button");
 let gridSize = 16;
+const minSize = 4;
+const maxSize = 100;
 
-for (let i = 0; i < gridSize; i++) {
-  for (let j = 0; j < gridSize; j++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    container.appendChild(square);
+gridSizeButton.addEventListener("click", () => {
+  const newResolution = prompt("Enter new resolution (16 - 100)");
+  if ((newResolution) => minSize && newResolution <= maxSize) {
+    generateGrid(newResolution);
+  } else {
+    alert(`Please enter a value between ${minSize} and ${maxSize}`);
   }
-}
+});
+
+const generateGrid = (gridSize) => {
+  container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      container.appendChild(square);
+    }
+  }
+};
+
+generateGrid(gridSize);
